50f83f622d575d5d7953cd31b32727b32711f5a7
Karim Salhi <karimsalhi@Karims-MacBook-Air.local>
2023-11-11 22:55:04 +0100

    Resultat page

--- game-erac/src/components/Appendicite/Result/Result.tsx
+++ game-erac/src/components/Appendicite/Result/Result.tsx
@@ -1,3 +1,48 @@
+import { Box, Button, Grid, List, Slider, Typography } from "@mui/material";
+import { orange } from "@mui/material/colors";
+import { useNavigate } from "react-router-dom";
+
 export default function Result() {
-  return <div>Result</div>;
+  const navigate = useNavigate();
+  return (
+    <Grid
+      container
+      sx={{
+        spacing: 0,
+        direction: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      style={{ minHeight: "100vh" }}
+    >
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper",
+          borderRadius: "5%",
+        }}
+        component="nav"
+        aria-label="mailbox folders"
+      >
+        <Box sx={{ width: 300 }}>
+          <Typography variant="h6">
+            Votre score est de
+            <Typography
+              variant="h5"
+              color="orange"
+              sx={{ alignItems: "center", justifyContent: "center" }}
+            >
+              945
+            </Typography>
+            Bravo vous faites partie du top
+            <Typography variant="h5" color="purple">
+              15%
+            </Typography>
+          </Typography>
+        </Box>
+        <Button onClick={() => navigate("/")}>Aller au menu</Button>
+      </List>
+    </Grid>
+  );
 }
